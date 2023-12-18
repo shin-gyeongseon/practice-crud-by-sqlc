11205d99af5ad5e8b400addd6b0c6658b6c09568
ShinGyeongseon <tlsrid1119@gmail.com>
2023-12-19 00:27:49 +0900

    add go paseto

---
+++ token/jwt_maker.go
@@ -0,0 +1,61 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+const (
+	minSecretKeySize = 32
+)
+
+type JWTMaker struct {
+	scretKey string
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size : must be at least %d characters", minSecretKeySize)
+	}
+
+	return &JWTMaker{secretKey}, nil
+}
+
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload) // Q. 이게 어떻게 토큰을 생성한다는걸 알아야할까 ? 
+	return jwtToken.SignedString([]byte(maker.scretKey))
+}
+
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrExpiredToken
+		}
+		return []byte(maker.scretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
---
+++ token/paseto_maker.go
@@ -0,0 +1,57 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aead/chacha20poly1305"
+	"github.com/o1egl/paseto"
+)
+
+type PasetoMaker struct {
+	paseto      *paseto.V2
+	symmeticKey []byte
+}
+
+func NewPasetoMaker(symmeticKey string) (Maker, error) {
+	if len(symmeticKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be examtly %d characters", chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		paseto:      paseto.NewV2(),
+		symmeticKey: []byte(symmeticKey),
+	}
+
+	return maker, nil
+}
+
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	token, err := maker.paseto.Encrypt(maker.symmeticKey, payload, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmeticKey, payload, nil)
+
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
